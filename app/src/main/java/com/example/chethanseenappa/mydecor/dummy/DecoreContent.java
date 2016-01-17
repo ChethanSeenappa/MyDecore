9d5a327ce495c168204e2943c25b914431679e92
chethan.seenappa <chethancse07@gmail.com>
2016-01-17 23:29:24 +0530

    Initial version

---
+++ app/src/main/java/com/example/chethanseenappa/mydecor/dummy/DecoreContent.java
@@ -0,0 +1,71 @@
+package com.example.chethanseenappa.mydecor.dummy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Helper class for providing sample content for user interfaces created by
+ * Android template wizards.
+ * <p>
+ * TODO: Replace all uses of this class before publishing your app.
+ */
+public class DecoreContent {
+    public static final List<DummyItem> ITEMS = new ArrayList<DummyItem>();
+    public static final Map<String, DummyItem> ITEM_MAP = new HashMap<String, DummyItem>();
+    private static final ArrayList<String> itemCategory = new ArrayList<String>();
+//  Replace static content with Dynamic content's of Decore
+    private static final int COUNT = 3;
+    static {
+        addItemCategory();
+        for (int i = 1; i <= COUNT; i++) {
+            addItem(createDummyItem(i));
+        }
+    }
+
+    private static void addItemCategory() {
+        itemCategory.add("Painting");
+        itemCategory.add("Home Decore");
+        itemCategory.add("Others");
+    }
+
+    //  Make content adder dynamic
+    private static void addItem(DummyItem item) {
+        ITEMS.add(item);
+        ITEM_MAP.put(item.id, item);
+    }
+
+    private static DummyItem createDummyItem(int position) {
+        return new DummyItem(String.valueOf(position), itemCategory.get(position-1), makeDetails(position));
+    }
+
+    private static String makeDetails(int position) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Details about Item: ").append(position);
+        for (int i = 0; i < position; i++) {
+            builder.append("\nMore details information here.");
+        }
+        return builder.toString();
+    }
+
+    /**
+     * A dummy item representing a piece of content.
+     */
+    public static class DummyItem {
+        public final String id;
+        public final String content;
+        public final String details;
+
+        public DummyItem(String id, String content, String details) {
+            this.id = id;
+            this.content = content;
+            this.details = details;
+        }
+
+        @Override
+        public String toString() {
+            return content;
+        }
+    }
+}
